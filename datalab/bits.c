13bb2a0c586224b9944e06c8be1640feac27b6cb
Raquel D <raqueldaviwek28@hotmail.com>
2024-09-20 12:51:11 -0300

    implemento byteSwap

--- datalab/bits.c
+++ datalab/bits.c
@@ -210,7 +210,25 @@ int anyOddBit(int x) {
  *  Rating: 2
  */
 int byteSwap(int x, int n, int m) {
-    return 2;
+    //creo mascara para el primer byte
+    int maskByte0 = ~(~0x00 << 8);
+
+    //encuentro las mascaras para los bits del swap
+    int maskByteM = maskByte0 << (m << 3);
+    int maskByteN =  maskByte0 << (n << 3);
+
+    //Extraigo bytes n y m y los posiciono en 0 y elimino posibles replicados del msb !0
+    int byteM = ((x & maskByteM) >> (m << 3)) & maskByte0;
+    int byteN = ((x & maskByteN) >> (n << 3)) & maskByte0;
+
+    //conservo el resto de posiciones que no hacen swap
+    int maskRestantes = ~(maskByteM | maskByteN);
+    int bytesRestantes = x & maskRestantes;
+
+    //colocar bytes extraidos en sus nuevas posiciones
+    int nuevoByteM = byteM << (n << 3);
+    int nuevoByteN = byteN << (m << 3);
+    return nuevoByteM | nuevoByteN | bytesRestantes;
 }
 /* 
  * fitsBits - return 1 if x can be represented as an 
