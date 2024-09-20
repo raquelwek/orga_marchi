64f33e2542e2047eed727254c1245f540ec1094b
victoria gomez <vegomez@fi.uba.ar>
2024-09-20 19:20:39 -0300

    Implemento fitsBits

--- datalab/bits.c
+++ datalab/bits.c
@@ -222,17 +222,9 @@ int byteSwap(int x, int n, int m) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-  /* x se podrá representar sii: -2^n-1 <= x <= (2^n-1) -1 */
-  
-  int shift = 0x1 << (n-1); // 2^n-1
-  int lim_sup = shift + (~0x1 + 1); // (2^n-1) - 1
-  int lim_inf = ~shift + 1; // -2^n-1
+  int aux_x = x >> (n + (~0x1 + 1)); // x >> (n - 1)
   int signo_x = x >> 31; 
-
-  int suma_sup_msb = (x + lim_sup) >> (31 - n); 
-  int suma_inf_msb = (x + lim_inf) >> (31- n);
-
-  return (!signo_x & suma_sup_msb) | (signo_x & !suma_inf_msb);
+  return !((aux_x ^ signo_x)); 
 }
 /* 
  * oddBits - return word with all odd-numbered bits set to 1
@@ -264,7 +256,7 @@ int sign(int x) {
  *   Rating: 3
  */
 int addOK(int x, int y) {
-  return 2;
+    return 2;
 }
 /* 
  * bitMask - Generate a mask consisting of all 1's 
